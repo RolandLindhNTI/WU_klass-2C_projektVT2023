8227c8a3d4deefcbfc305e1c05a4a3bb99b1d152
Roland Lindh NTI <118815577+RolandLindhNTI@users.noreply.github.com>
2023-02-21 14:50:38 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-const burger = document.getElementById('burger')
+const burger = document.getElementById('menu')
 const subpageslist = document.getElementById('subpages_list')
 
 
 function toggleMenu() {
     subpageslist.classList.toggle('subpages_list_visible')
 
-    burger.classList.toggle('burger_x')
+    burger.classList.toggle('menu_x')
 }
 
 burger.addEventListener('click', toggleMenu)
